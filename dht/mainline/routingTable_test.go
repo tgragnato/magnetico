1301835282ddafc31f4bb9eb7490f77a514d06e8
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-03-19 11:50:31 +0100

    test(mainline): fix testing logic

--- dht/mainline/routingTable_test.go
+++ dht/mainline/routingTable_test.go
@@ -17,9 +17,9 @@ func Test_routingTable_isEmpty(t *testing.T) {
 
 	t.Run("empty adding port 0", func(t *testing.T) {
 		rt := newRoutingTable(1)
-		rt.addNode(net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 1234})
-		if rt.isEmpty() {
-			t.Error("expected non-empty routing table")
+		rt.addNode(net.UDPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 0})
+		if !rt.isEmpty() {
+			t.Error("expected empty routing table")
 		}
 	})
 
