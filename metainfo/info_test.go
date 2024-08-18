7237a5992601e0bc7d4e2562b0e5975f42e45c31
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-08-19 00:40:01 +0200

    test(metainfo): improve coverage of info

--- metainfo/info_test.go
+++ metainfo/info_test.go
@@ -3,14 +3,64 @@ package metainfo
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/tgragnato/magnetico/bencode"
 )
 
 func TestMarshalInfo(t *testing.T) {
+	t.Parallel()
+
 	var info Info
 	b, err := bencode.Marshal(info)
-	assert.NoError(t, err)
-	assert.EqualValues(t, "d4:name0:12:piece lengthi0e6:pieces0:e", string(b))
+	if err != nil {
+		t.Errorf("Error marshaling info: %v", err)
+	}
+	expected := "d4:name0:12:piece lengthi0e6:pieces0:e"
+	if string(b) != expected {
+		t.Errorf("Unexpected marshaled value. Expected: %s, Got: %s", expected, string(b))
+	}
+}
+
+func TestTotalLength(t *testing.T) {
+	t.Parallel()
+
+	info := Info{
+		Files: []FileInfo{
+			{Length: 100},
+			{Length: 200},
+			{Length: 300},
+		},
+	}
+
+	expected := int64(600)
+	actual := info.TotalLength()
+
+	if expected != actual {
+		t.Errorf("Unexpected total length. Expected: %d, Got: %d", expected, actual)
+	}
+}
+
+func TestIsDir(t *testing.T) {
+	t.Parallel()
+
+	// Test when Info.Files is empty
+	info := Info{}
+	expected := false
+	actual := info.IsDir()
+	if expected != actual {
+		t.Errorf("Unexpected result. Expected: %v, Got: %v", expected, actual)
+	}
+
+	// Test when Info.Files is not empty
+	info = Info{
+		Files: []FileInfo{
+			{Length: 100},
+			{Length: 200},
+			{Length: 300},
+		},
+	}
+	expected = true
+	actual = info.IsDir()
+	if expected != actual {
+		t.Errorf("Unexpected result. Expected: %v, Got: %v", expected, actual)
+	}
 }
