ca6e0e192e5d0729292eee763cc59300c5ce7244
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-08-31 22:43:53 +0200

    test(persistence): add unit testing for postgres.GetTorrent

--- persistence/postgres_test.go
+++ persistence/postgres_test.go
@@ -1,11 +1,13 @@
 package persistence
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/sha1"
 	mrand "math/rand"
 	"testing"
 	"text/template"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 )
@@ -166,6 +168,79 @@ func TestPostgresDatabase_Close(t *testing.T) {
 	err = db.Close()
 	if err != nil {
 		t.Error(err)
+	}
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestPostgresDatabase_GetTorrent(t *testing.T) {
+	t.Parallel()
+
+	conn, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	var random [100]byte
+	_, err = rand.Read(random[:])
+	if err != nil {
+		for i := 0; i < 100; i++ {
+			random[i] = byte(mrand.Intn(256))
+		}
+	}
+	infohash := sha1.Sum(random[:])
+	name := "Test Torrent"
+	size := uint64(1024)
+	discoveredOn := time.Now().Unix()
+
+	rows := sqlmock.NewRows([]string{"info_hash", "name", "total_size", "discovered_on", "n_files"}).
+		AddRow(infohash[:], name, size, discoveredOn, 5)
+	mock.ExpectQuery("SELECT t.info_hash, t.name, t.total_size, t.discovered_on, \\(SELECT COUNT\\(\\*\\) FROM files f WHERE f.torrent_id = t.id\\) AS n_files FROM torrents t WHERE t.info_hash = \\$1;").
+		WithArgs(infohash[:]).
+		WillReturnRows(rows)
+
+	db := &postgresDatabase{conn: conn}
+	torrent, err := db.GetTorrent(infohash[:])
+	if err != nil {
+		t.Error(err)
+	}
+	if torrent == nil {
+		t.Fatal("Expected torrent to be found, but got nil")
+	}
+
+	if !bytes.Equal(torrent.InfoHash, infohash[:]) {
+		t.Errorf("Expected InfoHash to be %v, but got %v", infohash, torrent.InfoHash)
+	}
+	if torrent.Name != name {
+		t.Errorf("Expected Name to be %q, but got %q", name, torrent.Name)
+	}
+	if torrent.Size != size {
+		t.Errorf("Expected Size to be %d, but got %d", size, torrent.Size)
+	}
+	if torrent.DiscoveredOn != discoveredOn {
+		t.Errorf("Expected DiscoveredOn to be %v, but got %v", discoveredOn, torrent.DiscoveredOn)
+	}
+	if torrent.NFiles != 5 {
+		t.Errorf("Expected NFiles to be 5, but got %d", torrent.NFiles)
+	}
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Error(err)
+	}
+
+	rows = sqlmock.NewRows([]string{"info_hash", "name", "total_size", "discovered_on", "n_files"})
+	mock.ExpectQuery("SELECT t.info_hash, t.name, t.total_size, t.discovered_on, \\(SELECT COUNT\\(\\*\\) FROM files f WHERE f.torrent_id = t.id\\) AS n_files FROM torrents t WHERE t.info_hash = \\$1;").
+		WithArgs(infohash[:]).
+		WillReturnRows(rows)
+
+	torrent, err = db.GetTorrent(infohash[:])
+	if err != nil {
+		t.Error(err)
+	}
+	if torrent != nil {
+		t.Error("Expected torrent to be not found, but got a result")
 	}
 	if err := mock.ExpectationsWereMet(); err != nil {
 		t.Error(err)
