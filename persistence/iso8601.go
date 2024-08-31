7e43e105f2dfec60429efa9625a0f6f4a14c22df
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-08-31 21:15:26 +0200

    fix(persistence): return an invalid value - avoid panics

--- persistence/iso8601.go
+++ persistence/iso8601.go
@@ -7,11 +7,13 @@ import (
 	"time"
 )
 
-var yearRE = regexp.MustCompile(`^(\d{4})$`)
-var monthRE = regexp.MustCompile(`^(\d{4})-(\d{2})$`)
-var weekRE = regexp.MustCompile(`^(\d{4})-W(\d{2})$`)
-var dayRE = regexp.MustCompile(`^(\d{4})-(\d{2})-(\d{2})$`)
-var hourRE = regexp.MustCompile(`^(\d{4})-(\d{2})-(\d{2})T(\d{2})$`)
+var (
+	yearRE  = regexp.MustCompile(`^(\d{4})$`)
+	monthRE = regexp.MustCompile(`^(\d{4})-(\d{2})$`)
+	weekRE  = regexp.MustCompile(`^(\d{4})-W(\d{2})$`)
+	dayRE   = regexp.MustCompile(`^(\d{4})-(\d{2})-(\d{2})$`)
+	hourRE  = regexp.MustCompile(`^(\d{4})-(\d{2})-(\d{2})T(\d{2})$`)
+)
 
 type Granularity int
 
@@ -27,79 +29,38 @@ const (
 // date, nor it's intended to be, since we don't need that.
 func ParseISO8601(s string) (*time.Time, Granularity, error) {
 	if matches := yearRE.FindStringSubmatch(s); len(matches) != 0 {
-		year, err := parseYear(matches[1])
-		if err != nil {
-			return nil, -1, err
-		}
-
+		year := parseYear(matches[1])
 		t := time.Date(year, time.December, daysOfMonth(time.December, year), 23, 59, 59, 0, time.UTC)
 		return &t, Year, nil
 	}
 
 	if matches := monthRE.FindStringSubmatch(s); len(matches) != 0 {
-		month, err := parseMonth(matches[2])
-		if err != nil {
-			return nil, -1, err
-		}
-		year, err := parseYear(matches[1])
-		if err != nil {
-			return nil, -1, err
-		}
-
+		month := parseMonth(matches[2])
+		year := parseYear(matches[1])
 		t := time.Date(year, month, 31, 23, 59, 59, 0, time.UTC)
 		return &t, Month, nil
 	}
 
 	if matches := weekRE.FindStringSubmatch(s); len(matches) != 0 {
-		week, err := parseWeek(matches[2])
-		if err != nil {
-			return nil, -1, err
-		}
-		year, err := parseYear(matches[1])
-		if err != nil {
-			return nil, -1, err
-		}
-
+		week := parseWeek(matches[2])
+		year := parseYear(matches[1])
 		t := time.Date(year, time.January, week*7, 23, 59, 59, 0, time.UTC)
 		return &t, Week, nil
 	}
 
 	if matches := dayRE.FindStringSubmatch(s); len(matches) != 0 {
-		month, err := parseMonth(matches[2])
-		if err != nil {
-			return nil, -1, err
-		}
-		year, err := parseYear(matches[1])
-		if err != nil {
-			return nil, -1, err
-		}
-		day, err := parseDay(matches[3], daysOfMonth(month, year))
-		if err != nil {
-			return nil, -1, err
-		}
-
+		month := parseMonth(matches[2])
+		year := parseYear(matches[1])
+		day := parseDay(matches[3], daysOfMonth(month, year))
 		t := time.Date(year, month, day, 23, 59, 59, 0, time.UTC)
 		return &t, Day, nil
 	}
 
 	if matches := hourRE.FindStringSubmatch(s); len(matches) != 0 {
-		month, err := parseMonth(matches[2])
-		if err != nil {
-			return nil, -1, err
-		}
-		year, err := parseYear(matches[1])
-		if err != nil {
-			return nil, -1, err
-		}
-		hour, err := parseHour(matches[4])
-		if err != nil {
-			return nil, -1, err
-		}
-		day, err := parseDay(matches[3], daysOfMonth(month, year))
-		if err != nil {
-			return nil, -1, err
-		}
-
+		month := parseMonth(matches[2])
+		year := parseYear(matches[1])
+		hour := parseHour(matches[4])
+		day := parseDay(matches[3], daysOfMonth(month, year))
 		t := time.Date(year, month, day, hour, 59, 59, 0, time.UTC)
 		return &t, Hour, nil
 	}
@@ -138,7 +99,7 @@ func daysOfMonth(month time.Month, year int) int {
 	case time.December:
 		return 31
 	default:
-		panic("invalid month!")
+		return 0
 	}
 }
 
@@ -154,52 +115,42 @@ func isLeap(year int) bool {
 	}
 }
 
-func atoi(s string) int {
-	i, e := strconv.Atoi(s)
-	if e != nil {
-		// panic on error since atoi() will be called only after we parse it with regex
-		// (hopefully `\d`!)
-		panic(e.Error())
+func parseYear(s string) int {
+	year, err := strconv.Atoi(s)
+	if err != nil || year <= 1583 {
+		return 0
 	}
-	return i
+	return year
 }
 
-func parseYear(s string) (int, error) {
-	year := atoi(s)
-	if year <= 1583 {
-		return 0, fmt.Errorf("years before 1583 are not allowed")
+func parseMonth(s string) time.Month {
+	month, err := strconv.Atoi(s)
+	if err != nil || month <= 0 || month >= 13 {
+		return time.Month(-1)
 	}
-	return year, nil
+	return time.Month(month)
 }
 
-func parseMonth(s string) (time.Month, error) {
-	month := atoi(s)
-	if month <= 0 || month >= 13 {
-		return time.Month(-1), fmt.Errorf("month is not in range [01, 12]")
+func parseWeek(s string) int {
+	week, err := strconv.Atoi(s)
+	if err != nil || week <= 0 || week >= 54 {
+		return -1
 	}
-	return time.Month(month), nil
+	return week
 }
 
-func parseWeek(s string) (int, error) {
-	week := atoi(s)
-	if week <= 0 || week >= 54 {
-		return -1, fmt.Errorf("week is not in range [01, 53]")
+func parseDay(s string, max int) int {
+	day, err := strconv.Atoi(s)
+	if err != nil || day <= 0 || day > max {
+		return -1
 	}
-	return week, nil
+	return day
 }
 
-func parseDay(s string, max int) (int, error) {
-	day := atoi(s)
-	if day <= 0 || day > max {
-		return -1, fmt.Errorf("day is not in range [01, %d]", max)
+func parseHour(s string) int {
+	hour, err := strconv.Atoi(s)
+	if err != nil || hour <= -1 || hour >= 25 {
+		return -1
 	}
-	return day, nil
-}
-
-func parseHour(s string) (int, error) {
-	hour := atoi(s)
-	if hour <= -1 || hour >= 25 {
-		return -1, fmt.Errorf("hour is not in range [00, 24]")
-	}
-	return hour, nil
+	return hour
 }
