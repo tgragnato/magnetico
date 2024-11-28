e048a84d3c9df69fa851c0dfabb06acb20b9dc18
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2024-11-28 01:39:04 +0100

    test(opflags): write more test cases for OpFlags.parseYaml()

--- opflags/parsers_test.go
+++ opflags/parsers_test.go
@@ -7,11 +7,33 @@ import (
 func TestParseYaml(t *testing.T) {
 	t.Parallel()
 
-	o := &OpFlags{
+	tests := []struct {
+		name           string
+		ConfigFilePath string
+		expectError    bool
+	}{{
+		name:           "ValidConfigFilePath",
 		ConfigFilePath: "../doc/config.example.yml",
+		expectError:    false,
+	}, {
+		name:           "EmptyConfigFilePath",
+		ConfigFilePath: "",
+		expectError:    false,
+	}, {
+		name:           "InvalidConfigFilePath",
+		ConfigFilePath: "invalid-path",
+		expectError:    true,
+	},
 	}
 
-	if err := o.parseYaml(); err != nil {
-		t.Fatal(err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			o := &OpFlags{
+				ConfigFilePath: tt.ConfigFilePath,
+			}
+			if err := o.parseYaml(); (err != nil) != tt.expectError {
+				t.Errorf("OpFlags.parseYaml() error = %v, expectError %v", err, tt.expectError)
+			}
+		})
 	}
 }
