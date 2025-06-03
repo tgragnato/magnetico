f4bf572b2b78b421df16c851fc661d05c91bfd0c
Tommaso Gragnato <gragnato.tommaso@icloud.com>
2025-06-03 15:37:04 +0200

    perf(persistence): implement partitioning for files table in postgresql

--- persistence/postgres.go
+++ persistence/postgres.go
@@ -21,14 +21,17 @@ import (
 )
 
 type postgresDatabase struct {
-	conn *sql.DB
+	conn        *sql.DB
+	isCockroach bool
 }
 
 func makePostgresDatabase(url_ *url.URL) (Database, error) {
 	db := new(postgresDatabase)
+	db.isCockroach = false
 
 	if url_.Scheme == "cockroach" {
 		url_.Scheme = "postgres"
+		db.isCockroach = true
 	}
 
 	var err error
@@ -563,11 +566,53 @@ func (db *postgresDatabase) setupDatabase() error {
 	switch schemaVersion {
 	case 0: // FROZEN.
 		log.Println("Updating database schema from 0 to 1... (this might take a while)")
-		_, err = tx.Exec(`
-			CREATE INDEX IF NOT EXISTS idx_files_path_gin_trgm ON files USING GIN (path gin_trgm_ops);
-			INSERT INTO migrations (schema_version) VALUES (1);
-		`)
-		if err != nil {
+		migrationStmt := `
+				-- Rename the existing table
+				ALTER TABLE files RENAME TO files_old;
+
+				-- Create the new partitioned table
+				CREATE TABLE files (
+					id          INTEGER PRIMARY KEY DEFAULT nextval('seq_files_id'),
+					torrent_id  INTEGER REFERENCES torrents ON DELETE CASCADE ON UPDATE RESTRICT,
+					size        BIGINT NOT NULL,
+					path        TEXT NOT NULL
+				) PARTITION BY HASH (id);
+
+				-- Create the partitions
+				DO $$
+				BEGIN
+					FOR i IN 0..99 LOOP
+						EXECUTE format(
+							'CREATE TABLE files_p%s PARTITION OF files FOR VALUES WITH (MODULUS 100, REMAINDER %s);',
+							i, i
+						);
+						EXECUTE format(
+							'CREATE INDEX IF NOT EXISTS idx_files_torrent_id_p%s ON files_p%s (torrent_id);',
+							i, i
+						);
+						EXECUTE format(
+							'CREATE INDEX IF NOT EXISTS idx_files_path_gin_trgm_p%s ON files_p%s USING GIN (path gin_trgm_ops);',
+							i, i
+						);
+					END LOOP;
+				END$$;
+
+				-- Copy data from the old table to the new partitioned table
+				INSERT INTO files (id, torrent_id, size, path) SELECT id, torrent_id, size, path FROM files_old;
+
+				-- Drop the old indexes, table and finalize the migration
+				DROP INDEX IF EXISTS idx_files_torrent_id;
+				DROP TABLE IF EXISTS files_old;
+				INSERT INTO migrations (schema_version) VALUES (1);
+			`
+		if db.isCockroach {
+			migrationStmt = `
+				CREATE INDEX IF NOT EXISTS idx_files_path_gin_trgm ON files USING GIN (path gin_trgm_ops);
+				INSERT INTO migrations (schema_version) VALUES (1);
+			`
+		}
+
+		if _, err := tx.Exec(migrationStmt); err != nil {
 			return errors.New("sql.Tx.Exec (v0 -> v1) " + err.Error())
 		}
 
